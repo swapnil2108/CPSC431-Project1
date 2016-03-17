f364a68c109b55458fb8e68629b762a716a43d03
karthik006 <karthikknidhi@csu.fullerton.edu>
2016-03-17 01:16:40 -0700

    HTml page with loaded json

---
+++ reddit.js
@@ -0,0 +1,26 @@
+var main = function() {
+    "use strict";
+    function myfunction() {
+        $.get("http://localhost:3000/reddit", function(getData) {
+			  getData.forEach(function(reddit) {
+             
+			 var postsList ="<div class ='postContent'>"
+							+"<div class = 'votes'>"
+			                +"<img class='voteUpButton' src='image/up.png'>"+"<br>"
+                            +"<strong id ="+reddit.id+" class='votesNum'>"+"</strong>"+"<br>"
+                            +"<img class='voteDownButton' src='image/down.png'>"
+							+"</div>"
+							+"<div class ='Content-List'>"
+							+"<p>"+"<a href="+JSON.stringify(reddit.main_link)+">"+reddit.link_title+"</a>"
+							+"</p>"
+							+"</div>";
+              
+			  $(postsList).appendTo('div.postsContainer');
+			  $("#"+reddit.id+"").text(reddit.likes);
+              });
+	 
+     myfunction();
+	  
+	  
+  };
+    $(document).ready(main);
